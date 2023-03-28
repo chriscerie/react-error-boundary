ad0619b9053b87011be3729ab76887c25cd82332
Christopher Chang <51393127+chriscerie@users.noreply.github.com>
2023-03-27 23:15:15 -0700

    Built and published by Moonwave

---
+++ assets/js/dfa207b8.797048f0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[678],{1132:e=>{e.exports=JSON.parse('{"functions":[],"properties":[],"types":[],"name":"Error","desc":"A `LuauPolyFill.Error` object to represent errors produced.\\n```lua\\n{\\n\\tmessage: string,\\n\\tname: string,\\n\\tstack: string,\\n}\\n```","source":{"line":19,"path":"src/types.lua"}}')}}]);
