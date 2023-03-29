5ca05eae94414c6a5b1094161a05d0a05bbaedd7
Christopher Chang <51393127+chriscerie@users.noreply.github.com>
2023-03-28 20:03:43 -0700

    Fix withErrorBoundary docs

--- src/withErrorBoundary.lua
+++ src/withErrorBoundary.lua
@@ -5,10 +5,15 @@ local ErrorBoundary = require(script.Parent.ErrorBoundary)
 local types = require(script.Parent.types)
 
 --[=[
-	This is a higher-order component that makes it easy to add an error boundary to an existing component. See
+    This is a higher-order component that makes it easy to add an error boundary to an existing component. See
     [ErrorIn1SecondHOC](https://github.com/chriscerie/roact-error-boundary/blob/main/stories/ErrorIn1SecondHOC.story.lua) for a full example.
 
-	@within ReactErrorBoundary
+    :::note
+    If using luau strict mode, the component must be typed using `local Component: React.FC<{ <prop-types> }> = function(props)`
+    for types to work.
+    :::
+
+    @within ReactErrorBoundary
 ]=]
 local function withErrorBoundary<Props>(component: React.ComponentType<Props>, errorBoundaryProps: types.ErrorBoundaryProps)
 	local Wrapped = React.forwardRef(function(props: Props, ref)
