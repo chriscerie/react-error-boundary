7a8e3632adf091611ba479f87ca3cf50580600eb
chriscerie <51393127+chriscerie@users.noreply.github.com>
2023-03-29 04:44:56 +0000

    Deploying to gh-pages from @ chriscerie/react-error-boundary@90d2633cc0f0e1cbc3c1af5eaf41bdda7e55df50 🚀

--- assets/js/24e71142.b59191c8.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[653],{1132:e=>{e.exports=JSON.parse('{"functions":[],"properties":[],"types":[],"name":"Error","desc":"A `LuauPolyFill.Error` object to represent errors produced.\\n```lua\\n{\\n\\tmessage: string,\\n\\tname: string,\\n\\tstack: string,\\n}\\n```","source":{"line":19,"path":"src/types.lua"}}')}}]);
